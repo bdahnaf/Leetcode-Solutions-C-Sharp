f7cabed1fd0cb7c0a4af7b8b5516c01aed5a144b
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-07-23 15:27:11 +0600

    Added main driver project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Leetcode_Solutions_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is the main solution");
+            Console.WriteLine("Just right click on your desired project and choose 'Set as Startup Project'");
+        }
+    }
+}
