c02b0bbb7dc393fbd8ef424d830790bf181943c2
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-07-25 15:18:05 +0600

    WIP

--- LC_2160/Program.cs
+++ LC_2160/Program.cs
@@ -5,11 +5,29 @@
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.MinimumSum(1000);
+            Console.WriteLine(program.MinimumSum(1000));
         }
         public int MinimumSum(int num)
         {
-            return 0;
+            int smallest = 0;
+            int secondSmallest = 0;
+            int digits_sum = 0;
+            while (num != 0)
+            {
+                int digits = num % 10;
+                if(digits < smallest)
+                {
+                    secondSmallest = smallest;
+                    smallest = digits;
+                }
+                else if (digits < secondSmallest)
+                {
+                    secondSmallest = digits;
+                }
+                digits_sum += digits;
+                num /= 10;
+            }
+            return digits_sum;
         }
     }
 }
