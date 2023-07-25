d7cf97f11d87d4b668492b3343939e062fe2be0b
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-07-25 13:49:52 +0600

    Trying out problem no. 2160

---
+++ LC_2160/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LC_2160
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.MinimumSum(1000);
+        }
+        public int MinimumSum(int num)
+        {
+            return 0;
+        }
+    }
+}
