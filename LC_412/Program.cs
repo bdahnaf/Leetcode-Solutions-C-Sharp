9e584442de7562e667b292a3f97fcec9a75d0c8e
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-06 12:15:06 +0600

    WA on input 1

--- LC_412/Program.cs
+++ LC_412/Program.cs
@@ -5,11 +5,42 @@
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.FizzBuzz(3);
+            IList<string> list = program.FizzBuzz(10000);
+            foreach (string item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
         public IList<string> FizzBuzz(int n)
         {
             IList<string> list = new List<string>();
+            if(n < 3)
+            {
+                list.Add("1");
+                list.Add("2");
+                return list;
+            }
+            list.Add("1");
+            list.Add("2");
+            for (int i = 3; i <= n; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    list.Add("FizzBuzz");
+                    continue;
+                }
+                if(i % 3 == 0)
+                {
+                    list.Add("Fizz");
+                    continue;
+                }
+                if (i % 5 == 0)
+                {
+                    list.Add("Buzz");
+                    continue;
+                }
+                list.Add(i.ToString());
+            }
             return list;
         }
     }
