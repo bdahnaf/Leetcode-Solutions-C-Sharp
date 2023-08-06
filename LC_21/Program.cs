54903ed0d945af0030605b11ce8601aa98394b90
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-06 15:57:08 +0600

    Added the driver program

--- LC_21/Program.cs
+++ LC_21/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+            ListNode list1 = new ListNode(1);
+            list1.next = new ListNode(3);
+            list1.next.next = new ListNode(5);
+
+            ListNode list2 = new ListNode(2);
+            list2.next = new ListNode(4);
+            list2.next.next = new ListNode(6);
+
+            program.MergeTwoLists(list1, list2);
+        }
+        
+
+         
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode mergedList = new ListNode();
+            return mergedList;
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
         }
     }
 }
