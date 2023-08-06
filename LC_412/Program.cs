edfb9fee84963f5d23bdbc7c5d5b049347badbda
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-06 12:05:36 +0600

    Trying out problem no. 412

---
+++ LC_412/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LC_412
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public IList<string> FizzBuzz(int n)
+        {
+            IList<string> list = new List<string>();
+            return list;
+        }
+    }
+}
