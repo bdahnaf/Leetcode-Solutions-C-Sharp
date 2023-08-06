1d2cb6752f5a30b11403a641fde3bd49c17117bb
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-06 12:40:23 +0600

    Solved LC problem no. 1342

--- LC_1342/Program.cs
+++ LC_1342/Program.cs
@@ -5,11 +5,27 @@
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.NumberOfSteps(6);
+            int ans = program.NumberOfSteps(14);
+            Console.WriteLine(ans);
         }
         public int NumberOfSteps(int num)
         {
-            return 0;
+            int rem = 0;
+            int counter = 0;
+            while (num != 0)
+            {
+                if(num % 2 == 0)
+                {
+                    rem = num % 2;
+                    num = num / 2;
+                } 
+                else
+                {
+                    num = num - 1;
+                }
+                counter++;
+            }
+            return counter;
         }
     }
 }
