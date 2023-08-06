9d0776977f3ba3cae20d389dcb1579edb6736e9c
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-06 15:36:48 +0600

    Solved LC problem no. 20

--- LC_20/Program.cs
+++ LC_20/Program.cs
@@ -5,10 +5,27 @@
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.IsValid("");
+            bool isValid = program.IsValid("()()()((");
+            Console.WriteLine(isValid);
         }
         public bool IsValid(string s)
         {
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (stack.Count > 0 && c == ')' && stack.Peek() == '(')
+                    stack.Pop();
+                else if (stack.Count > 0 && c == '}' && stack.Peek() == '{')
+                    stack.Pop();
+                else if (stack.Count > 0 && c == ']' && stack.Peek() == '[')
+                    stack.Pop();
+                else
+                    stack.Push(c);
+            }
+            if(stack.Count == 0)
+            {
+                return true;
+            }
             return false;
         }
     }
