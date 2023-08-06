542d891d724894c73059a528368b3feb3c189994
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-06 14:44:20 +0600

    Trying out problem no. 20

---
+++ LC_20/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LC_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public bool IsValid(string s)
+        {
+            return false;
+        }
+    }
+}
