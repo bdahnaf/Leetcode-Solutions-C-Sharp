456a2daf4ffe10590facf0872ceaf2d3e156d1f7
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-07 23:32:25 +0600

    Solved LC problem no. 2798

--- LC_2798/Program.cs
+++ LC_2798/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            Program program = new Program();
+            int[] hours = { 0, 1, 2, 3, 4 };
+            int target = 2;
+            int ans = program.NumberOfEmployeesWhoMetTarget(hours, target);
+            Console.WriteLine(ans);
+        }
+        public int NumberOfEmployeesWhoMetTarget(int[] hours, int target)
+        {
+            int ans = 0;
+            for(int i = 0; i < hours.Length; i++)
+            {
+                if (hours[i] >= target)
+                {
+                    ans++;
+                }
+            }
+            return ans;
         }
     }
 }
