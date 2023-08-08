3464a26cc5fb8cbb4c435b19b9274cba367ed552
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-08 23:23:21 +0600

    Solved problem no 2652

--- LC_2652/Program.cs
+++ LC_2652/Program.cs
@@ -5,11 +5,19 @@
         static void Main(string[] args)
         {
             Program program = new Program();
-            Console.WriteLine(program.SumOfMultiples(7));
+            Console.WriteLine(program.SumOfMultiples(9));
         }
         public int SumOfMultiples(int n)
         {
-            return 0;
+            int sum = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                if(i % 3 == 0 || i % 5 == 0 || i % 7 ==0)
+                {
+                    sum = sum + i;
+                }
+            }
+            return sum;
         }
     }
 }
