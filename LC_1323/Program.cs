1359563b454b2602f5da1ab96cc1267d9136b709
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-09 13:50:00 +0600

    Trying out LC problem no. 1323

---
+++ LC_1323/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LC_1323
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public int Maximum69Number(int num)
+        {
+            return num;
+        }
+    }
+}
