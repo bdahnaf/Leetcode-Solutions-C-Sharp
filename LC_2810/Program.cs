ba4580882782efadf0f9275edd9387469bc1a5b0
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-09 13:26:54 +0600

    Solved problem no. 2810

--- LC_2810/Program.cs
+++ LC_2810/Program.cs
@@ -4,11 +4,33 @@
     {
         static void Main(string[] args)
         {
-            
+            Program program = new Program();
+            Console.WriteLine(program.FinalString("string"));
         }
         public string FinalString(string s)
         {
-
+            string ans = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != 'i')
+                {
+                    ans = ans + s[i];
+                }
+                else
+                {
+                    ans = Reverse(ans);
+                }
+            }
+            return ans;
+        }
+        public string Reverse(string s)
+        {
+            string revString = "";
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                revString += s[i];
+            }
+            return revString;
         }
     }
 }
