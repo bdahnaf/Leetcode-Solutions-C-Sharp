469b964b062130e5e84714de567de2dde7e295f2
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-09 09:01:58 +0600

    Trying out new LC problem no. 1859

---
+++ LC_1859/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LC_1859
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public string SortSentence(string s)
+        {
+            return s;
+        }
+    }
+}
