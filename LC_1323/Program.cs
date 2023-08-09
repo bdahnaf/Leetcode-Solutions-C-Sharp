10378642a5ffa2c422877ffb912dad9550e81540
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-09 14:06:26 +0600

    Solved LC problem no 1323

--- LC_1323/Program.cs
+++ LC_1323/Program.cs
@@ -5,11 +5,33 @@
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.Maximum69Number(9969);
+            Console.WriteLine(program.Maximum69Number(9669));
         }
         public int Maximum69Number(int num)
         {
-            return num;
+            int rem = 0;
+            List<int> list = new List<int>();
+            while(num > 0)
+            {
+                rem = num % 10;
+                num = num / 10;
+                list.Add(rem);
+            }
+            list.Reverse();
+            for(int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == 6)
+                {
+                    list[i] = 9;
+                    break;
+                }
+            }
+            int total = 0;
+            foreach(int i in list)
+            {
+                total = 10 * total + i;
+            }
+            return total;
         }
     }
 }
