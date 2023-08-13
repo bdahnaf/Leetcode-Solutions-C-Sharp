ad76ce02857fbe993373129dcb16174d76bd759d
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-13 16:06:55 +0600

    Added instruction on the main project

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("This is the main solution");
             Console.WriteLine("Just right click on your desired project and choose 'Set as Startup Project'");
+            Console.WriteLine("Make sure to right click on the solution and then click on rebuild solution!");
         }
     }
 }
