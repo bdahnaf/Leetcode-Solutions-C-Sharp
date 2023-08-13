570a1a76ac2e77a132241f7ffbe809f318ad2c56
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-13 23:05:21 +0600

    Solved LC problem no. 1389

--- LC_1389/Program.cs
+++ LC_1389/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+            int[] nums = { 0, 1, 2, 3, 4 };
+            int[] index = { 0, 1, 2, 2, 1 };
+            int[] ans = program.CreateTargetArray(nums, index);
+            foreach (int i in ans)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            List<int> list = new List<int>();
+            for(int i =0; i < nums.Length; i++)
+            {
+                list.Insert(index[i], nums[i]);
+            }
+            return list.ToArray();
         }
     }
 }
