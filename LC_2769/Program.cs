49b03d5605e44e4ca45ea30f442a9ee7ac6fd327
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-13 16:11:05 +0600

    Trying out LC problem no. 2769

---
+++ LC_2769/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LC_2769
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public int TheMaximumAchievableX(int num, int t)
+        {
+            return 0;
+        }
+    }
+}
