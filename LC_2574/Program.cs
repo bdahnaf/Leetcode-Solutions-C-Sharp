c8fe50e4b0291e1bf3d90868009a1fc467b2633f
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-13 16:30:04 +0600

    Trying out LC problem no. 2574

---
+++ LC_2574/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LC_2574
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public int[] LeftRightDifference(int[] nums)
+        {
+            int[] ans = new int[nums.Length];
+            return ans;
+        }
+    }
+}
