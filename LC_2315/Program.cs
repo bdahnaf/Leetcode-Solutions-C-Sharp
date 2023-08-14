8531f2e3d0e313f6e4c0bb167154d17a9fa30469
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-14 19:46:51 +0600

    Trying out new LC problem no. 2315

---
+++ LC_2315/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LC_2315
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public int CountAsterisks(string s)
+        {
+            return 0;
+        }
+    }
+}
