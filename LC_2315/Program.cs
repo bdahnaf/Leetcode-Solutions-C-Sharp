23e302e0f04babf89957e65de0144155a5b2b394
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-14 20:17:02 +0600

    WIP

--- LC_2315/Program.cs
+++ LC_2315/Program.cs
@@ -4,9 +4,33 @@
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+            program.CountAsterisks("l|*e*et|c**o|*de|");
         }
         public int CountAsterisks(string s)
         {
+            int ans = 0;
+            int firstP = 0;
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '|' && firstP == 1)
+                {
+                    continue;
+                }
+                else if (s[i] == '|')
+                {
+                    firstP = 1;
+                }
+                else if (firstP == 1 && s[i] == '*')
+                {
+                    continue;
+                }
+                else if (s[i] == '*')
+                {
+                    ans++;
+                }
+            }
+            Console.WriteLine(ans);
             return 0;
         }
     }
