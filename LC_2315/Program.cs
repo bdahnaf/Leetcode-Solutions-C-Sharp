e28461a6ccf8204d93b423a5ab3f3b15d3280b3b
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-16 14:39:13 +0600

    Solved LC problem no 2315

--- LC_2315/Program.cs
+++ LC_2315/Program.cs
@@ -5,32 +5,34 @@
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.CountAsterisks("l|*e*et|c**o|*de|");
+            program.CountAsterisks("yo|uar|e**|b|e***au|tifu|l");
         }
         public int CountAsterisks(string s)
         {
             int ans = 0;
             int firstP = 0;
+            int sAns = 0;
             for(int i = 0; i < s.Length; i++)
             {
                 if (s[i] == '|' && firstP == 1)
                 {
-                    continue;
+                    firstP = 0;
+                    ans = ans - sAns;
+                    sAns = 0;
                 }
-                else if (s[i] == '|')
+                else if (s[i] == '|' && firstP == 0)
                 {
                     firstP = 1;
-                }
-                else if (firstP == 1 && s[i] == '*')
-                {
-                    continue;
                 }
                 else if (s[i] == '*')
                 {
                     ans++;
+                    if (firstP == 1)
+                    {
+                        sAns++;
+                    }
                 }
             }
-            Console.WriteLine(ans);
             return ans;
         }
     }
