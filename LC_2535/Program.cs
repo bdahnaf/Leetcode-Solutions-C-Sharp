be9e3c63540e2d30e8dea8aaa11081c38731607c
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-16 14:52:08 +0600

    Solved LC problem no. 2535

--- LC_2535/Program.cs
+++ LC_2535/Program.cs
@@ -5,12 +5,31 @@
         static void Main(string[] args)
         {
             Program program = new Program();
-            int[] nums = { 1, 15, 6, 3 };
-            program.DifferenceOfSum(nums);
+            int[] nums = { 1, 2, 3, 4 };
+            Console.WriteLine(program.DifferenceOfSum(nums));
         }
         public int DifferenceOfSum(int[] nums)
         {
-            return 0;
+            int eSum = 0;
+            int dSum = 0;
+            for(int i = 0; i < nums.Length; i++) 
+            {
+                eSum += nums[i];
+                while (nums[i] > 0)
+                {
+                    int digit = nums[i] % 10;
+                    dSum += digit;
+                    nums[i] = nums[i] / 10;
+                }
+            }
+            if(eSum >= dSum)
+            {
+                return eSum - dSum;
+            }
+            else
+            {
+                return dSum - eSum;
+            }
         }
     }
 }
