03571ac1baf849e6a061fd6dd84be8c5a5e1408e
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-16 14:47:17 +0600

    Added the driver program

--- LC_2535/Program.cs
+++ LC_2535/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+            int[] nums = { 1, 15, 6, 3 };
+            program.DifferenceOfSum(nums);
+        }
+        public int DifferenceOfSum(int[] nums)
+        {
+            return 0;
         }
     }
 }
