611fa2c2da301da0fe2514ff894ed662f1bf4721
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-16 15:17:09 +0600

    Added the driver program

--- LC_2006/Program.cs
+++ LC_2006/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+            int[] nums = { 1, 2, 2, 1 };
+            int k = 1;
+            program.CountKDifference(nums, k);
+        }
+        public int CountKDifference(int[] nums, int k)
+        {
+            return 0;
         }
     }
 }
