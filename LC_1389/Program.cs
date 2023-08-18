fba6915a9dfd14e862db1a3f1a957727df1ef34a
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-18 22:08:11 +0600

    AC

--- LC_1389/Program.cs
+++ LC_1389/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Program program = new Program();
-            int[] nums = { 0, 1, 2, 3, 4, 9 , 10 };
+            int[] nums = { 0, 1, 2, 3, 4 };
             int[] index = { 0, 1, 2, 2, 1 };
             int[] ans = program.CreateTargetArray(nums, index);
             foreach (int i in ans)
