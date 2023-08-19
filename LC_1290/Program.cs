2cc936536e1ec85c08d4a5543d98d8f0e5532e50
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-19 23:35:10 +0600

    Added the driver program

--- LC_1290/Program.cs
+++ LC_1290/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+            ListNode node3 = new ListNode(1);
+            ListNode node2 = new ListNode(0, node3);
+            ListNode node1 = new ListNode(1, node2);
+            int decimalValue = program.GetDecimalValue(node1);
+        }
+        public int GetDecimalValue(ListNode head)
+        {
+            return 0;
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
         }
     }
 }
