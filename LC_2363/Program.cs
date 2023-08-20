d97303ed318195b5ffa03a35f1bbe63821a36c36
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-20 23:09:13 +0600

    Added the driver program

--- LC_2363/Program.cs
+++ LC_2363/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+            int[][] items1 = new int[3][];
+            items1[0] = new int[2];
+            items1[1] = new int[2];
+            items1[2] = new int[2];
+            items1[0][0] = 1;
+            items1[0][1] = 1;
+            items1[1][0] = 4;
+            items1[1][1] = 5;
+            items1[2][0] = 3;
+            items1[2][1] = 8;
+
+            int[][] items2 = new int[2][];
+            items2[0] = new int[2];
+            items2[1] = new int[2];
+            items2[0][0] = 3;
+            items2[0][1] = 1;
+            items2[1][0] = 1;
+            items2[1][1] = 5;
+
+            program.MergeSimilarItems(items1, items2);
+        }
+        public IList<IList<int>> MergeSimilarItems(int[][] items1, int[][] items2)
+        {
+            IList<IList<int>> ret = new List<IList<int>>();
+            return ret;
         }
     }
 }
