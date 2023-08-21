5fb66fb7e7b0ce82f998f1a5f821cf66373b1aae
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-21 23:11:02 +0600

    Added the driver program

--- LC_2586/Program.cs
+++ LC_2586/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] words = { "are", "amy", "u" };
+            int left = 0;
+            int right = 2;
+            Program program = new Program();
+            Console.WriteLine(program.VowelStrings(words, left, right));
+        }
+        public int VowelStrings(string[] words, int left, int right)
+        {
+            return 0;
         }
     }
 }
