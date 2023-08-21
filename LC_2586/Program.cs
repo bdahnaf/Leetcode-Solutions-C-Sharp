f3682f80f4ab7bf4b5d69b536ffb844842ad3e36
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-21 23:20:01 +0600

    Solved LC problem no. 2586

--- LC_2586/Program.cs
+++ LC_2586/Program.cs
@@ -4,19 +4,28 @@
     {
         static void Main(string[] args)
         {
-            string[] words = { "are", "amy", "u" };
-            int left = 0;
-            int right = 2;
+            string[] words = { "hey", "aeo", "mu", "ooo", "artro" };
+            int left = 1;
+            int right = 4;
             Program program = new Program();
             Console.WriteLine(program.VowelStrings(words, left, right));
         }
         public int VowelStrings(string[] words, int left, int right)
         {
-            for(int i = left; i <= right; i++)
+            int ans = 0;
+            for (int i = left; i <= right; i++)
             {
-                Console.WriteLine(words[i][0]);
+                char firstChar = words[i][0];
+                char lastChar = words[i][words[i].Length - 1];
+                if(firstChar == 'a' || firstChar == 'e' || firstChar == 'i' || firstChar == 'o' || firstChar == 'u')
+                {
+                    if(lastChar == 'a' || lastChar == 'e' || lastChar == 'i' || lastChar == 'o' || lastChar == 'u')
+                    {
+                        ans++;
+                    }
+                }
             }
-            return 0;
+            return ans;
         }
     }
 }
