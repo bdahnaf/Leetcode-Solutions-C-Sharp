abc0844f91a979d68ec481bc2ebf75c4676ae77f
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-23 23:20:43 +0600

    Solved LC problem no. 2169

--- LC_2169/Program.cs
+++ LC_2169/Program.cs
@@ -5,11 +5,27 @@
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.CountOperations(2, 3);
+            Console.WriteLine(program.CountOperations(2, 3));
         }
         public int CountOperations(int num1, int num2)
         {
             int ans = 0;
+            while(true)
+            {
+                if(num1 == 0 || num2 == 0)
+                {
+                    break;
+                }
+                if (num1 >= num2)
+                {
+                    num1 = num1 - num2;
+                }
+                else
+                {
+                    num2 = num2 - num1;
+                }
+                ans++;
+            }
             return ans;
         }
     }
