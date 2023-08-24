7a1e208c245ed5aa273111291c8a5df1dacc5669
Ahnaf Shahriar <bdahnaf@gmail.com>
2023-08-24 23:26:59 +0600

    Solved LC problem no 66

--- LC_66/Program.cs
+++ LC_66/Program.cs
@@ -4,7 +4,63 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] digits = { 8, 9 };
+            Program program = new Program();
+            int[] ans = program.PlusOne(digits);
+            foreach (int i in ans)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public int[] PlusOne(int[] digits)
+        {
+            List<int> result = new List<int>();
+            int carry = 0;
+            if(digits.Length == 1)
+            {
+                if (digits[0] == 9)
+                {
+                    result.Add(1);
+                    result.Add(0);
+                    return result.ToArray();
+                }
+                else
+                {
+                    result.Add(digits[0] + 1);
+                    return result.ToArray();
+                }
+            }
+            else
+            {
+                for(int i = digits.Length - 1; i <= 0; i--)
+                {
+                    Console.WriteLine(digits[i]);
+                }
+                //foreach (int digit in digits)
+                //{
+                //    if (digit == 9)
+                //    {
+                //        result.Add(0);
+                //        carry = 1;
+                //    }
+                //    else
+                //    {
+                //        if(carry == 1)
+                //        {
+                //            result.Add(digit+1);
+                //        }
+                //        else
+                //        {
+                //            result.Add(digit);
+                //        }
+                //    }
+                //}
+                if(carry == 1)
+                {
+                    result.Add(1);
+                }
+            }
+            return result.ToArray();
         }
     }
 }
